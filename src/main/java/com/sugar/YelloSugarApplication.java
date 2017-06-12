ee56bec80ffd5b8aa4b1117b311d4b4c22b4b319
{hongseongmin} <{hnsnmn@gmail.com}>
2017-06-12 23:07:59 +0900

    first commit

---
+++ src/main/java/com/sugar/YelloSugarApplication.java
@@ -0,0 +1,12 @@
+package com.sugar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YelloSugarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YelloSugarApplication.class, args);
+	}
+}
